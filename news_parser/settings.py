4f0760ebbfadd71e65fdcc1b33c4affba6678f21
snaumov <s.s.naumov@gmail.com>
2016-08-04 12:03:34 +0300

    minor

--- news_parser/settings.py
+++ news_parser/settings.py
@@ -41,7 +41,7 @@ except:
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['debug']
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
